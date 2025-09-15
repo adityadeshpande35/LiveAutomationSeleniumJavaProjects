63f9b819131f52d94bb1a6403fe48b63f00693c1
Aditya Deshpande <adityadeshpande35@gmail.com>
2025-09-15 23:31:48 +0530

    newFiles

---
+++ src/test/java/tutorialsninja/register/TC_RF_006.java
@@ -0,0 +1,45 @@
+package tutorialsninja.register;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Date;
+
+public class TC_RF_006 {
+
+    @Test
+    public void verifyRegisteringAccountBySayingNoToNewsletter()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("John");
+        driver.findElement(By.id("input-lastname")).sendKeys("Wick");
+        driver.findElement(By.id("input-email")).sendKeys(gernarateNewEmail());
+        driver.findElement(By.id("input-telephone")).sendKeys("9876543210");
+        driver.findElement(By.id("input-password")).sendKeys("John@123");
+        driver.findElement(By.id("input-confirm")).sendKeys("John@123");
+        driver.findElement(By.xpath("//input[@name='newsletter' and @value='0']")).click();
+        driver.findElement(By.name("agree")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        driver.findElement(By.xpath("//a[text()='Continue']")).click();
+        driver.findElement(By.linkText("Subscribe / unsubscribe to newsletter")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Newsletter']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@name='newsletter' and @value='0' and @checked]")).isSelected());
+driver.quit();
+
+    }
+    public String gernarateNewEmail() {
+
+        return new Date().toString().replace(" ", "").replace(":", "") + "@gmail.com";
+
+    }
+}
